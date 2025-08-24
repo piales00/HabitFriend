f872bffd9e8efa35ee33739dd71927abcba86483
piales00 <perez.izquierdo.piero@gmail.com>
2025-08-24 15:03:30 -0500

    agregación de tailwindcss

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h2 class="text-3xl font-bold underline">Página de login</h2>
+    </div>
+  );
+}
+export default Login;
